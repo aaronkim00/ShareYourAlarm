1a88cffb255bce574a95c0fb7609ff5f2e440564
aaronkim00 <aaron-kim@naver.com>
2021-11-23 17:11:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.todolist
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mainFragment: Fragment
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mainFragment = MainFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.container, mainFragment).commit()
+        val saveButton: Button = findViewById(R.id.saveButton)
+        Log.d("hello", "oh")
+        saveButton.setOnClickListener {
+            saveToDo()
+            Toast.makeText(applicationContext, "추가되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun saveToDo() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/todolist/Note.kt
@@ -0,0 +1,27 @@
+package com.example.todolist
+
+class Note {
+    private var _id = 0
+    private var todo: String? = null
+
+    fun Note(_id: Int, todo: String?) {
+        this._id = _id
+        this.todo = todo
+    }
+
+    fun get_id(): Int {
+        return _id
+    }
+
+    fun set_id(_id: Int) {
+        this._id = _id
+    }
+
+    fun getTodo(): String? {
+        return todo
+    }
+
+    fun setTodo(todo: String?) {
+        this.todo = todo
+    }
+}
