61f38f4a581af4a3a9eb526eb723303b301fdc30
aaronkim00 <aaron-kim@naver.com>
2021-11-29 21:24:07 +0900

    2

---
+++ app/src/main/java/com/example/shareyouralarm/DestinationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.shareyouralarm
+
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class DestinationActivity : AppCompatActivity() {
+}
